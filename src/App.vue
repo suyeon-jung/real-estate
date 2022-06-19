eacb8194fd50e6e597871af05308c54e90615997
suyeon-jung <wjdjsy210@gmail.com>
2022-06-19 12:11:54 +0900

    [feat] : (4) 이벤트 핸들러

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,40 @@
         <a v-for="(menu, idx) in menus" :key="idx">{{menu}}</a>
     </div>
 
-  <div v-for="(product, idx) in products" :key="idx">
-      <h4>{{product}}</h4>
+  <div>
+      <h4>{{products[0]}}</h4>
       <p>{{prices[0]}}</p>
+      <button @click="increase(0)">허위매물 신고</button> <span>신고수 : {{신고수[0]}}</span>
   </div>
+  <div>
+      <h4>{{products[1]}}</h4>
+      <p>{{prices[1]}}</p>
+      <button @click="increase(1)">허위매물 신고</button> <span>신고수 : {{신고수[1]}}</span>
 
+  </div>
+  <div>
+      <h4>{{products[2]}}</h4>
+      <p>{{prices[2]}}</p>
+      <button @click="increase(2)">허위매물 신고</button> <span>신고수 : {{신고수[2]}}</span>
+  </div> 
 </template>
 
 <script>
+
 
 export default {
   name: 'App',
   data(){
       return {
+          신고수 : [0, 0, 0],
           menus : ["Home", "Shop", "About"],
           products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
           prices: ["50만원", "60만원", "70만원"]
+      }
+  },
+  methods:{
+      increase(idx){
+          this.신고수[idx] += 1;
       }
   },
   components: {
