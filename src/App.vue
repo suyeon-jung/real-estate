2e5871775a86392b5a707dd56676d0fb926505de
suyeon-jung <wjdjsy210@gmail.com>
2022-06-21 00:06:11 +0900

    [feat] : (7) HTML 컴포넌트화  
     

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-    <div class="menu">
-        <a v-for="(menu, idx) in menus" :key="idx">{{menu}}</a>
-    </div>
+    <navigation-bar />
 
     <div class="black-bg" v-if="모달창">
         <div class="white-bg">
@@ -11,56 +9,45 @@
         </div>
     </div>
 
-  <div v-for="(product, idx) in products" :key="idx">
-      <img :src="product.image" class="room-img">
-      <h4 @click="모달창 = true">{{product.title}}</h4>
-      <p>{{product.price}}원</p>
-  </div>
+    <div v-for="(product, idx) in products" :key="idx">
+        <img :src="product.image" class="room-img">
+        <h4 @click="모달창 = true">{{ product.title }}</h4>
+        <p>{{ product.price }}원</p>
+    </div>
 </template>
 
 <script>
 import products from "./assets/data";
+import NavigationBar from "./components/NavigationBar.vue";
 
 
 export default {
-  name: 'App',
-  data(){
-      return {
-          모달창 : false,
-          menus : ["Home", "Shop", "About"],
-          products: products,
-          prices: ["50만원", "60만원", "70만원"]
-      }
-  },
-  methods:{
-      increase(idx){
-          this.신고수[idx] += 1;
-      }
-  },
-  components: {
-
-  }
+    name: 'App',
+    data() {
+        return {
+            모달창: false,
+            products: products,
+            prices: ["50만원", "60만원", "70만원"]
+        }
+    },
+    methods: {
+        increase(idx) {
+            this.신고수[idx] += 1;
+        }
+    },
+    components: {
+        NavigationBar
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-.menu {
-    background: darkslateblue;
-    padding: 15px;
-    border-radius:  5px;
-}
-
-.menu a {
-    color: white;
-    padding:  10px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 .room-img {
@@ -77,13 +64,15 @@ div {
 }
 
 .black-bg {
-    width: 100%; height: 100%;
-    background: rgba(0,0,0,0.5);
-    position: fixed; padding: 20px;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    padding: 20px;
 }
 
 .white-bg {
-    width: 100%; 
+    width: 100%;
     background: white;
     border-radius: 8px;
     padding: 20px;
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="menu">
+        <a v-for="(menu, idx) in menus" :key="idx">{{ menu }}</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "navigation-bar",
+    data() {
+        return {
+            menus: ["Home", "Shop", "About"],
+        }
+    },
+    methods: {
+
+    }
+}
+
+</script>
+
+<style>
+.menu {
+    background: darkslateblue;
+    padding: 15px;
+    border-radius: 5px;
+}
+
+.menu a {
+    color: white;
+    padding: 10px;
+}
+</style>
